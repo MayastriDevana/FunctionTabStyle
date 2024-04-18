fc1f0a621e7543abca4bb1bfcebc756bbe7022fe
Maya <Mayastridevana0205@gmail.com>
2024-04-18 10:01:40 +0700

    Initial Commit

---
+++ FunctionTabStyle/FunctionTabStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionTabStyleApp.swift
+//  FunctionTabStyle
+//
+//  Created by User on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FunctionTabStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
